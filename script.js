26b19a498befb25444d4800d76fff604e0a3e01a
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-11-01 04:54:08 +0330

    js  ready
    final

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const counters = document.querySelectorAll('.counter');
 
+counters.forEach((count) => {
+  count.innerText = '0';
+
+  const updateCounter = () => {
+    const target = +count.getAttribute('data-target');
+    const curentNum = +count.innerText;
+
+    const increment = target / 200;
+
+    if (curentNum < target) {
+      count.innerText = `${Math.ceil(curentNum + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCounter();
+});
